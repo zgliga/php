1591b63801ed1fb84ceb49604738dd6624403425
zgliga <133985683+zgliga@users.noreply.github.com>
2024-04-16 00:35:15 +0800

    Create 11.bulletin.php

---
+++ 11.bulletin.php
@@ -0,0 +1,32 @@
+<?php
+//用SESSION結合資料庫登入
+    error_reporting(0);
+    session_start();
+    if (!$_SESSION["id"]) {
+        echo "請先登入";
+        echo "<meta http-equiv=REFRESH content='3, url=2.login.html'>";
+    }
+    else{
+        echo "歡迎, ".$_SESSION["id"]."[<a href=12.logout.php>登出</a>] [<a href=18.user.php>管理使用者</a>] [<a href=22.bulletin_add_form.php>新增佈告</a>]<br>";
+        $conn=mysqli_connect("db4free.net", "immust", "immustimmust", "immust");
+        $result=mysqli_query($conn, "select * from bulletin");
+        echo "<table border=2><tr><td></td><td>佈告編號</td><td>佈告類別</td><td>標題</td><td>佈告內容</td><td>發佈時間</td></tr>";
+        while ($row=mysqli_fetch_array($result)){
+            echo "<tr><td><a href=26.bulletin_edit_form.php?bid={$row["bid"]}>修改</a> 
+            <a href=28.bulletin_delete.php?bid={$row["bid"]}>刪除</a></td><td>";
+            echo $row["bid"];
+            echo "</td><td>";
+            echo $row["type"];
+            echo "</td><td>"; 
+            echo $row["title"];
+            echo "</td><td>";
+            echo $row["content"]; 
+            echo "</td><td>";
+            echo $row["time"];
+            echo "</td></tr>";
+        }
+        echo "</table>";
+    
+    }
+
+?>
