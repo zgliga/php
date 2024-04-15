b281573e04c047912e88a87d2042ee73964fa636
zgliga <133985683+zgliga@users.noreply.github.com>
2024-04-15 23:12:03 +0800

    Create 1.html.php

---
+++ 1.html.php
@@ -0,0 +1,13 @@
+<html>
+    <head><title>html+php</title></head>      
+    <body>
+        <?php echo "Hello world!"; ?> 
+        <h1>html</h1>
+        <?php echo "Hello world!"; ?> 
+        <h1>html</h1>
+        <?php echo "Hello world!"; ?> 
+    </body>
+</html>
+#PHP檔案通常用來搭配html網頁。再由HTML網頁，來呼叫PHP檔案，使伺服器執行後，將結果回傳給瀏覽器
+#當要呼叫PHP網頁程式，可以透過<form>來執行
+#例如：login.html 內 <form呼叫login.php> -> login.php執行 -結果回傳-> 瀏覽器
